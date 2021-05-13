ebf01874f70ff18d27cf8e299f3b6cbd7f24bdc3
koremandar967 <koremandar967@gmail.com>
2021-05-13 10:56:41 +0530

    language selector css modified

--- src/components/LangauageSelectionContainer.js
+++ src/components/LangauageSelectionContainer.js
@@ -9,17 +9,17 @@ export const LangauageSelectionContainer = () => {
                 <div className="text-center">
                     <h3 className="top-header">Add & Select Langauage</h3>
                     <p className="text-color-gray">Add langauages which you know <mark className="mark-box">(Min 2)</mark> and <mark className="mark-box">(Max 4)</mark> and select which is your native.</p>
-                    <ul className = "lang-list">
-                        <li className = "lang-list-li">Coffee</li>
-                        <li className = "lang-list-li">Tea</li>
-                        <li className = "lang-list-li">
-                            <a className = "add-new-lang lang-list-li-a">
+                    <ul className="lang-list">
+                        <li className="lang-list-li"><a className="lang-list-li-a"><span>✔</span>Coffee</a></li>
+                        <li className="lang-list-li"><a className="lang-list-li-a lang-list-li-a-selected"><span>✔</span>Tea</a></li>
+                        <li className="lang-list-li">
+                            <a className="add-new-lang lang-list-li-a">
                                 <label>+</label>
                                     Add Langauage
                                     </a>
-                            </li>
+                        </li>
                     </ul>
-
+                    <button className="btn btn-orange" name='make-list-button'>Let's make list of vocabulary</button>
                 </div>
             </div>
         </div>
