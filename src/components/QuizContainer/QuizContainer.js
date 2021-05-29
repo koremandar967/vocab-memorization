b6d891ccb6a546de2f7ba2da1875ef18d12dc9e9
koremandar967 <koremandar967@gmail.com>
2021-05-29 23:41:11 +0530

    pProgress bar step generation logic implemented

--- src/components/QuizContainer/QuizContainer.js
+++ src/components/QuizContainer/QuizContainer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './QuizContainer.css';
-
+import {StepProgressBar} from '../StepProgressBar/StepProgressBar'
 
 export  const QuizContainer = (props) => {
     return (
         <div className = "text-center">
             <h3 className="top-header">Quiz</h3>
             <p className="text-color-gray">Give answers of <mark className="mark-box">{props.questionLength + " questions."}</mark> You have to translate native language word into others langauage</p>
+            <StepProgressBar stepLength = {props.questionLength}/>
         </div>
     )
 }
---
+++ src/components/StepProgressBar/StepProgressBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './StepProgressBar.css';
+import "react-step-progress-bar/styles.css";
+import {ProgressBar,Step} from 'react-step-progress-bar';
+
+export const StepProgressBar = (props) => {
+ 
+  let transformedSteps = [];
+  
+  const getSteps = () => {
+    console.log(props.stepLength);
+    for(let i = 0; i<props.stepLength; i++) {
+      
+      transformedSteps.push(<Step key = {i} transition="scale">
+      {({ accomplished }) => (
+        <img
+          style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+          width="30"
+          src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+        />
+      )}
+    </Step>);
+    }
+
+    return transformedSteps;
+  }
+  return (
+    <div className = "progress-wrapper">
+      <ProgressBar
+        percent={75}
+        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+      >
+        {getSteps()}
+        
+      </ProgressBar>
+
+    </div>
+  );
+}
